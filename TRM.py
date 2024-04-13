b67fb249382eabf905c47878f071057c8a57ef88
Hu_J <38395602+HuJGithub@users.noreply.github.com>
2024-04-13 09:03:05 +0800

    Update and rename IDF.py to TRM.py

--- TRM.py
+++ TRM.py
@@ -4,7 +4,7 @@ import random
 from data_process.ProcessedData import ProcessedData
 
 
-class IDF(ProcessedData):
+class TRM(ProcessedData):
 
     def __init__(self, raw_data):
         super().__init__(raw_data)
@@ -15,18 +15,16 @@ class IDF(ProcessedData):
         DF = self.feature_df.sum(axis=0)
         print(DF)
 
-        # 计算文档总数
         N = len(self.feature_df)
         print(N)
 
-        # 计算每列的IDF
-        IDF = np.log(N / (DF))/DF**0.5
-        print(IDF)
-        # 遍历整个DataFrame，将每个元素乘以对应的IDF值
+        TRM = np.log(N / (DF))/DF**0.5
+        print(TRM)
+       
         for col in self.feature_df.columns:
-            self.feature_df[col] *= IDF[col]
+            self.feature_df[col] *= TRM[col]
 
-        # 打印修改后的DataFrame
+        
         print(self.feature_df)
         self.data_df = pd.concat([self.feature_df, self.label_df], axis=1)
         #print(self.data_df)
